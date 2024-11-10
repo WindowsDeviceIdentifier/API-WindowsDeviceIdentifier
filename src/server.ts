a3c929436fd9f2e91ad3a24b6029b9406ed1ca46
François-Pierre <francoispierrerousseau.44@gmail.com>
2024-11-10 09:51:26 +0100

    fix

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,7 @@ import express from "express";
 
 const app = express();
 
-const port = 8000;
-
-app.listen(port, () => {
+app.listen(process.env.PORT ?? 8000, () => {
   console.log(
     `Server started on port: http://localhost:${process.env.PORT ?? 8000}`
   );
