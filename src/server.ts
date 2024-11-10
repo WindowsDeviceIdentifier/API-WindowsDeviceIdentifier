ff2520c83e387826806f254a9163a7029337868c
François-Pierre <francoispierrerousseau.44@gmail.com>
2024-11-10 09:58:32 +0100

    fix

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,14 @@
-import express from "express";
+import express, { Router } from "express";
 
 const app = express();
+
+const router = Router();
+
+router.get("coucou", (req, res, next) => {
+  res.status(200).json({
+    response: "COUCOU",
+  });
+});
 
 app.listen(process.env.PORT ?? 8000, () => {
   console.log(
