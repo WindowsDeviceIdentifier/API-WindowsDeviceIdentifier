d45d23bd38b2336074cb158d28fef47a2197b478
François-Pierre <francoispierrerousseau.44@gmail.com>
2024-11-11 09:30:28 +0100

    maj

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,20 @@
 import express, { Router } from "express";
+import { auth } from "express-oauth2-jwt-bearer";
 
 const app = express();
 
 const router = Router();
 
-router.get("/coucou", (req, res, next) => {
+const jwtCheck = auth({
+  audience: process.env.AUTH0_AUDIENCE,
+  issuerBaseURL: process.env.AUTH0_ISSUER,
+  tokenSigningAlg: process.env.AUTH0_SIGNING_ALG,
+});
+
+app.use(jwtCheck);
+
+router.get("/signup", (req, res, next) => {
+  console.log(req);
   res.status(200).json({
     response: "COUCOU",
   });
